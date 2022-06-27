db2829d901726a24dd76bdc590d8fa424f901a00
sunrobbie <sunrobbie@comcast.net>
2022-06-27 15:35:00 -0400

    Added .make.sh, needs updated

---
+++ .make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#dir=~/.config
+#dotdir=~/dotfiles
+
+#ln -s $dotdir/picom.conf $dir/picom.conf
+#ln -s $dotdir/alacritty.yml $dir/alacritty.yml
+#ln -s $dotdir/rc.lua $dir/awesome/rc.lua
+#ln -s $dotdir/cava.config $dir/cava/config
+#ln -s $dotdir/config.rasi $dir/rofi/config.rasi
+
